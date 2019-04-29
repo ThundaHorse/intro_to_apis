824cb7b27619b9d803dba0477df09c1f95f7ff73
ThundaHorse <abeeeeekim@gmail.com>
2019-04-29 17:56:37 -0500

    cleaned up dictionary_app

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,48 +1,68 @@
 require 'http' 
 
 system "clear" 
-puts "Enter a word you wish to look up."
-word = gets.chomp 
 
-while word != 'q' 
-  # puts "=" * 50 
-  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=10&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd") 
+in_progress = true 
 
-  # examples = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+while in_progress
+  system "clear" 
+  puts "Dictionary, so you can get smarter" 
+  puts "=" * 60 
+  puts "" 
 
-  # pronounciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
+  puts "Enter a word you want to know." 
+  input_word = gets.chomp 
 
-  definition = response.parse
-  example_output = examples.parse 
-  # pronounc = pronounciation.parse 
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=10&includeRelated=false&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
 
-  puts definition[0]['text']
-  p "=" * 50 
-  # puts example_output['examples'][1]['text'] 
-  # p "=" * 50 
-  # puts pronounc[0]['raw']
-  # p "=" * 50 
+  definitions = response.parse 
 
-  puts "Enter a word you wish to look up." 
-  word = gets.chomp 
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/topExample?useCanonical=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+
+  top_example = response.parse["text"]
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/pronunciations?useCanonical=false&limit=5&typeFormat=IPA&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+
+  pronunciations = response.parse
+
+  puts "" 
+  puts " DEFINITIONS" 
+  puts "=" * 60 
+  puts "" 
+
+  count = 1 
+
+  definitions.each do |def_hash| 
+    puts "  #{count.to_s.rjust(3)}. #{def_hash['text']}."
+    count += 1 
+  end 
+
+  puts ""
+  puts "  TOP EXAMPLE"
+  puts "=" * 60
+  puts ""
+
+  puts top_example
+
+  puts ""
+  puts "  PRONOUNCIATIONS"
+  puts "=" * 60
+  puts ""
+
+  count = 1 
+  pronunciations.each do |pronounce_hash| 
+    puts "  •#{pronounce_hash['raw']}"
+  end 
+
+  puts "" 
+  puts "Press enter to continue or 'q' to quit" 
+  ending_input = gets.chomp 
+  if ending_input == 'q' 
+    in_progress = false 
+  end 
+
 end 
 
-
-# print "Enter a Word:" 
-# input_word = gets.chomp 
-
-# response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=3&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
-
-# definitions = response.parse 
-
-# puts "" 
-# puts "   DEFINITIONS" 
-# puts "=" * 60 
-# puts "" 
-
-# count = 1 
-
-# definitions.each do |def_hash| 
-#   puts "    #{count.to_s.rjust(3)}. #{def_hash['text']}" 
-#   count += 1 
-# end 
+system "clear" 
+puts "" 
+puts "BYE FELICIA" 
